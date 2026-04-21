05f3e34fa2390c7b58772122ac54db47e684958a
Bakdaule8 <satybaldybakdaulet425@gmail.com>
2026-04-21 21:46:25 +0500

    Add printArray method to Sorter class
    Added a printArray method to display array contents.

--- src/Sorter.java
+++ src/Sorter.java
@@ -55,4 +55,10 @@ class Sorter {
             arr[i] = (int)(Math.random() * 1000);
         return arr;
     }
+    
+    void printArray(int[] arr) {
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
 }
